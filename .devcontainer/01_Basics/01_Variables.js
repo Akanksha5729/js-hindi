eb94b4395524c20283635d1a17128081930c0c8b
Akanksha5729 <143889971+Akanksha5729@users.noreply.github.com>
2023-09-14 18:16:56 +0000

    this is variable and datatype

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "Ak123456@gmail.com"
+var accountCity = "Jaipur"
+let accountState;
+accountEmail = "ak@AD.com"
+accountPassword = "224433"
+accountCity = "Bengluru"
+//Prefer not to use var becouse of issue in block scope and functional scope
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

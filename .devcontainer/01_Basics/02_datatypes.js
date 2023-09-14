19a5a5e7207b76e6da17693a421353488e5cedb9
Akanksha5729 <143889971+Akanksha5729@users.noreply.github.com>
2023-09-14 18:31:22 +0000

    data type

--- .devcontainer/01_Basics/02_datatypes.js
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -2,3 +2,22 @@
 
 // alert(3+4) // we are using nodejs,  not browser
 
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "akanksha")
+
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
